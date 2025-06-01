a21ef4905dcd25038ebe0bc63856282bfeed3969
ytngmyo <myolieytng@gmail.com>
2025-06-01 13:46:32 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -18,5 +18,5 @@ export const SITE = {
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/HongKong", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Hong_Kong", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
